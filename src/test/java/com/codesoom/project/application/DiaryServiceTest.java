c640a9489c98e292a583ea1ed6e8799cca572b85
양효주 <yhyojoo@icloud.com>
2021-03-24 21:21:31 +0900

    다이어리 조회 테스트를 추가하라

--- src/test/java/com/codesoom/project/application/DiaryServiceTest.java
+++ src/test/java/com/codesoom/project/application/DiaryServiceTest.java
@@ -2,12 +2,15 @@ package com.codesoom.project.application;
 
 import com.codesoom.project.domain.Diary;
 import com.codesoom.project.domain.DiaryRepository;
+import com.codesoom.project.errors.DiaryNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -17,7 +20,8 @@ class DiaryServiceTest {
 
     private DiaryService diaryService;
 
-    private static final Long ID = 1L;
+    private static final Long INVALID_ID = 100L;
+    private static final Long VALID_ID = 1L;
     private static final String TITLE = "오늘의 다이어리";
     private static final String COMMENT = "아쉬운 하루였다";
 
@@ -33,12 +37,14 @@ class DiaryServiceTest {
         diaries = diaryService.getDiaries();
 
         diary = Diary.builder()
-                .id(ID)
+                .id(VALID_ID)
                 .title(TITLE)
                 .comment(COMMENT)
                 .build();
 
         given(diaryRepository.findAll()).willReturn(diaries);
+
+        given(diaryRepository.findById(VALID_ID)).willReturn(Optional.of(diary));
     }
 
     @Test
@@ -53,5 +59,21 @@ class DiaryServiceTest {
     @Test
     void getDiariesWithoutDiary() {
         assertThat(diaries).isEmpty();
+    }
+
+    @Test
+    void getDiaryWithValidId() {
+        diary = diaryService.getDiary(VALID_ID);
+
+        verify(diaryRepository).findById(VALID_ID);
+
+        assertThat(diary.getTitle()).isEqualTo(TITLE);
+        assertThat(diary.getComment()).isEqualTo(COMMENT);
+    }
+
+    @Test
+    void getDiaryWithInvalidId() {
+        assertThatThrownBy(() -> diaryService.getDiary(INVALID_ID))
+                .isInstanceOf(DiaryNotFoundException.class);
     }
 }
