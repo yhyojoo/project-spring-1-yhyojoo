e4a2a441ccfbc3a4c46ba6a1f261483c2b4e6703
양효주 <yhyojoo@icloud.com>
2021-03-25 19:24:06 +0900

    다이어리 수정 기능을 구현하라

--- src/main/java/com/codesoom/project/controllers/DiaryController.java
+++ src/main/java/com/codesoom/project/controllers/DiaryController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +42,7 @@ public class DiaryController {
      * 주어진 id에 해당하는 다이어리를 반환합니다.
      *
      * @param id 다이어리 식별자
-     * @return 주어진 id를 갖는 다이어리
+     * @return 다이어리
      */
     @GetMapping("{id}")
     public Diary detail(@PathVariable Long id) {
@@ -58,5 +59,19 @@ public class DiaryController {
     @ResponseStatus(HttpStatus.CREATED)
     public Diary create(@RequestBody @Valid DiaryData diaryData) {
         return diaryService.createDiary(diaryData);
+    }
+
+    /**
+     * 주어진 id에 해당하는 다이어리 정보를 수정합니다.
+     *
+     * @param id 다이어리 식별자
+     * @param diaryData 수정할 다이어리 정보
+     * @return 수정된 다이어리
+     */
+    @PatchMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Diary update(@PathVariable Long id,
+                        @RequestBody @Valid DiaryData diaryData) {
+        return diaryService.updateDiary(id, diaryData);
     }
 }
