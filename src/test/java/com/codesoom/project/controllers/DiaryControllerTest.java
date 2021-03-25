bdd069e1f4edbcc40d5e478608e6c125785a3305
양효주 <yhyojoo@icloud.com>
2021-03-25 16:39:30 +0900

    다이어리 상세 조회 테스트를 추가하라

--- src/test/java/com/codesoom/project/controllers/DiaryControllerTest.java
+++ src/test/java/com/codesoom/project/controllers/DiaryControllerTest.java
@@ -2,6 +2,9 @@ package com.codesoom.project.controllers;
 
 import com.codesoom.project.application.DiaryService;
 import com.codesoom.project.domain.Diary;
+import com.codesoom.project.domain.DiaryRepository;
+import com.codesoom.project.errors.DiaryNotFoundException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -9,13 +12,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(DiaryController.class)
@@ -26,11 +33,17 @@ class DiaryControllerTest {
     @MockBean
     private DiaryService diaryService;
 
+    @MockBean
+    private DiaryRepository diaryRepository;
+
+    private static final Long NOT_EXIST_ID = 100L;
     private static final Long ID = 1L;
     private static final String TITLE = "오늘의 다이어리";
     private static final String COMMENT = "아쉬운 하루였다";
 
     private Diary diary;
+    private Long givenValidId;
+    private Long givenInvalidId;
 
     @BeforeEach
     void setUp() {
@@ -45,6 +58,8 @@ class DiaryControllerTest {
         diaries.add(diary);
 
         given(diaryService.getDiaries()).willReturn(diaries);
+
+        given(diaryService.getDiary(ID)).willReturn(diary);
     }
 
     @Nested
@@ -67,5 +82,59 @@ class DiaryControllerTest {
                         .andExpect(status().isOk());
             }
         }
+    }
+
+    @Nested
+    @DisplayName("detail 메소드는")
+    class Describe_detail {
+
+        @Nested
+        @DisplayName("등록된 다이어리 id가 주어진다면")
+        class Context_with_valid_id {
+
+            @BeforeEach
+            void setUp() {
+                givenValidId = ID;
+            }
+
+            @Test
+            @DisplayName("주어진 id를 갖는 다이어리를 반환한다")
+            void it_returns_diary() throws Exception {
+                mockMvc.perform(get("/diaries/1")
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+                )
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString("아쉬운")));
+
+                verify(diaryService).getDiary(givenValidId);
+            }
+        }
+
+        @Nested
+        @DisplayName("등록되지 않은 다이어리 id가 주어진다면")
+        class Context_with_invalid_id {
+
+            @BeforeEach
+            void setUp() {
+                givenInvalidId = NOT_EXIST_ID;
+
+                given(diaryService.getDiary(NOT_EXIST_ID))
+                        .willThrow(new DiaryNotFoundException(NOT_EXIST_ID));
+            }
+
+            @Test
+            @DisplayName("주어진 id를 갖는 다이어리를 반환한다")
+            void it_returns_exception() throws Exception {
+                mockMvc.perform(get("/diaries/100"))
+                        .andExpect(status().isNotFound());
+
+                verify(diaryService).getDiary(givenInvalidId);
+            }
+        }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        diaryRepository.delete(diary);
     }
 }
