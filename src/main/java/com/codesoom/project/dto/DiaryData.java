25a7e6f84b1516c2cae2f6b72c0192b14767daed
양효주 <yhyojoo@icloud.com>
2021-03-25 18:00:03 +0900

    DiaryData의 title에 제약 조건을 추가하라

--- src/main/java/com/codesoom/project/dto/DiaryData.java
+++ src/main/java/com/codesoom/project/dto/DiaryData.java
@@ -1,8 +1,11 @@
 package com.codesoom.project.dto;
+
 import com.github.dozermapper.core.Mapping;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * 다이어리 DTO.
@@ -10,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class DiaryData {
+    @NotBlank(message = "다이어리 제목을 입력해 주세요.")
     @Mapping("title")
     private String title;
 
