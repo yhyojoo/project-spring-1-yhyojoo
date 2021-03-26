203f8e66a0eb6fc82502008cbf170ab3d0bb843f
양효주 <yhyojoo@icloud.com>
2021-03-26 16:23:33 +0900

    할 일 목록 조회 기능을 구현하라
    서비스와 컨트롤러에 목록 조회 기능을 구현합니다.

---
+++ src/main/java/com/codesoom/project/controllers/TaskController.java
@@ -0,0 +1,29 @@
+package com.codesoom.project.controllers;
+
+import com.codesoom.project.application.TaskService;
+import com.codesoom.project.domain.Task;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/diaries/{id}/tasks")
+@CrossOrigin
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    /**
+     * 전체 할 일 목록을 반환합니다.
+     *
+     * @return 전체 할 일 목록
+     */
+    @GetMapping
+    public List<Task> list() {
+        return taskService.getTasks();
+    }
+}
