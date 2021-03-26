ccdb6d8ff94cb58be5bf8e4988b82db969dee8af
양효주 <yhyojoo@icloud.com>
2021-03-26 17:43:07 +0900

    할 일 상세 조회 테스트를 작성하라
    할 일 상세 조회 기능에 대한 서비스와 컨트롤러 테스트를 작성합니다.

--- src/test/java/com/codesoom/project/application/TaskServiceTest.java
+++ src/test/java/com/codesoom/project/application/TaskServiceTest.java
@@ -2,14 +2,18 @@ package com.codesoom.project.application;
 
 import com.codesoom.project.domain.Task;
 import com.codesoom.project.domain.TaskRepository;
+import com.codesoom.project.errors.TaskNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -19,8 +23,14 @@ class TaskServiceTest {
 
     private TaskRepository taskRepository;
 
+    private static final Long NOT_EXIST_ID = 100L;
+    private static final Long ID = 1L;
+    private static final String TITLE = "첫 번째 할 일";
+
     private List<Task> tasks;
     private Task task;
+    private Long givenValidId;
+    private Long givenInvalidId;
 
     @BeforeEach
     void setUp() {
@@ -28,9 +38,16 @@ class TaskServiceTest {
 
         taskService = new TaskService(taskRepository);
 
+        task = Task.builder()
+                .id(ID)
+                .title(TITLE)
+                .build();
+
         tasks = taskService.getTasks();
 
         given(taskRepository.findAll()).willReturn(tasks);
+
+        given(taskRepository.findById(eq(ID))).willReturn(Optional.of(task));
     }
 
     @Nested
@@ -67,6 +84,49 @@ class TaskServiceTest {
                 verify(taskRepository).findAll();
 
                 assertThat(tasks).isEmpty();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("getTask 메소드는")
+    class Describe_getTask {
+
+        @Nested
+        @DisplayName("등록된 할 일의 id가 주어진다면")
+        class Context_with_valid_id {
+
+            @BeforeEach
+            void setUp() {
+                givenValidId = ID;
+            }
+
+            @Test
+            @DisplayName("주어진 id를 갖는 할 일을 반환한다")
+            void it_returns_task() {
+                task = taskService.getTask(givenValidId);
+
+                verify(taskRepository).findById(givenValidId);
+
+                assertThat(task.getId()).isEqualTo(ID);
+                assertThat(task.getTitle()).isEqualTo(TITLE);
+            }
+        }
+
+        @Nested
+        @DisplayName("등록되지 않은 할 일의 id가 주어진다면")
+        class Context_with_Invalid_id {
+
+            @BeforeEach
+            void setUp() {
+                givenInvalidId = NOT_EXIST_ID;
+            }
+
+            @Test
+            @DisplayName("조회할 할 일을 찾을 수 없다는 예외를 던진다")
+            void it_returns_exception() {
+                assertThatThrownBy(() -> taskService.getTask(givenInvalidId))
+                        .isInstanceOf(TaskNotFoundException.class);
             }
         }
     }
