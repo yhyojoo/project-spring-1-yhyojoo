68ac454ebb34828816ace0c41dbaa7fc8ce61478
양효주 <yhyojoo@icloud.com>
2021-03-26 17:44:20 +0900

    할 일 상세 조회 기능을 구현하라
    서비스와 컨트롤러에 상세 조회 기능을 구현합니다.

--- src/main/java/com/codesoom/project/application/TaskService.java
+++ src/main/java/com/codesoom/project/application/TaskService.java
@@ -2,6 +2,7 @@ package com.codesoom.project.application;
 
 import com.codesoom.project.domain.Task;
 import com.codesoom.project.domain.TaskRepository;
+import com.codesoom.project.errors.TaskNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +25,26 @@ public class TaskService {
      */
     public List<Task> getTasks() {
         return taskRepository.findAll();
+    }
+
+    /**
+     * 주어진 id에 해당하는 할 일을 반환합니다.
+     *
+     * @param id 할 일 식별자
+     * @return 주어진 id를 갖는 할 일
+     * @throws TaskNotFoundException 할 일을 찾을 수 없을 경우
+     */
+    public Task getTask(Long id) { return findTask(id);
+    }
+
+    /**
+     * 주어진 id에 해당하는 할 일을 반환합니다.
+     *
+     * @param id 할 일 식별자
+     * @return 주어진 id를 갖는 할 일
+     */
+    public Task findTask(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
     }
 }
--- src/main/java/com/codesoom/project/controllers/TaskController.java
+++ src/main/java/com/codesoom/project/controllers/TaskController.java
@@ -5,6 +5,7 @@ import com.codesoom.project.domain.Task;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,10 @@ public class TaskController {
     @GetMapping
     public List<Task> list() {
         return taskService.getTasks();
+    }
+
+    @GetMapping("{id}")
+    public Task detail(@PathVariable Long id) {
+        return taskService.getTask(id);
     }
 }
